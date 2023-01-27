4e723808c7157c6d066458e2d8aec7d8b3fed6b8
Victor Barros <victorclovisb@hotmail.com>
2023-01-27 11:31:28 -0300

    Updated type file on DiskStorage

--- src/providers/DiskStorage.ts
+++ src/providers/DiskStorage.ts
@@ -4,7 +4,7 @@ import fs from "fs"
 import { TMP_FOLDER, UPLOADS_FOLDER } from "../configs/upload"
 
 export class DiskStorage {
-  async saveFile(file) {
+  async saveFile(file: string) {
     await fs.promises.rename(
       path.resolve(TMP_FOLDER, file),
       path.resolve(UPLOADS_FOLDER, file)
@@ -13,7 +13,7 @@ export class DiskStorage {
     return file
   }
 
-  async deleteFile(file) {
+  async deleteFile(file: string) {
     const filePath = path.resolve(UPLOADS_FOLDER, file)
 
     try {
